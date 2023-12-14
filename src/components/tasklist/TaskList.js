4aa2d88617074e13d27db57c63a213c9d9c1484f
GenBernardo235 <147357699+GenBernardo235@users.noreply.github.com>
2023-12-14 15:02:33 +0800

    Update TaskList.js

--- src/components/tasklist/TaskList.js
+++ src/components/tasklist/TaskList.js
@@ -22,7 +22,7 @@ function Task({ task, handleUpdate, handleDelete }) {
   const [currentTask, setTask] = useState(task);
   useEffect(() => {
     handleUpdate(currentTask.id, currentTask);
-  }, [currentTask]);
+  }, [currentTask,handleUpdate]);
   return (
     <div className={currentTask.done ? 't done' : 't undone'}>
       <p>{currentTask.text}</p>
